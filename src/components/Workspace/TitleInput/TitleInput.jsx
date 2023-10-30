3271fe8ab08fa8abc19c6b1f4532f666dd0aac17
Wolferner <m.wolfern@yandex.com>
2023-10-30 14:08:48 +0200

    Comp:: Workspace: TitleInput-finished

--- src/components/Workspace/TitleInput/TitleInput.jsx
+++ src/components/Workspace/TitleInput/TitleInput.jsx
@@ -1,30 +1,49 @@
+import React, {useEffect} from 'react';
 import styles from './TitleInput.module.css'
+import M from 'materialize-css';
+
 
 const TitleInput = () =>{
+
+    useEffect(() => {
+        const options = {
+          data: {
+            "Apple": null,
+            "Google": null,
+            "Facebook": null}};
+    
+        const elems = document.querySelectorAll('.autocomplete');
+        M.Autocomplete.init(elems, options);
+      }, []);
+    
 
     return(
         <div className="row">
 
             <div className={`${styles.input} col s9 `}>
-                <div className={`${styles.titleBtn}`}>
-                    <a className="secondary-content prefix-button" href=""><i className="material-icons prefix">textsms</i></a>
-                    <a className="secondary-content prefix-button" href=""><i className="material-icons prefix">textsms</i></a>
-                </div>
+                <div className='row'>
+                    <div className={`${styles.titleBtn} col s1 `}>
+                        <a className="secondary-content prefix-button" href=""><i className="material-icons prefix">textsms</i></a>
+                        <a className="secondary-content prefix-button" href=""><i className="material-icons prefix">textsms</i></a>
+                    </div>
 
-                <div className="input-field ">
-                    <input type="text" id="autocomplete-input" className="autocomplete"/>
-                    <label htmlFor="autocomplete-input">Title</label>
+                    <div className={`${styles.inputCont} input-field col s11`}>
+                        <input type="text" id="autocomplete-input" className="autocomplete"/>
+                        <label htmlFor="autocomplete-input">Title</label>
+                    </div>
                 </div>
             </div>
 
             <div className='col s3 '>
-                <div className="row">
-                    <a href="" className="secondary-content"><i className="material-icons">stop</i></a>
-                    <a href="" className="secondary-content"><i
-                            className="material-icons">pause_circle_outline</i></a>
-                    <a href="" className="secondary-content"><i
-                            className="material-icons">play_circle_outline</i></a>
-                    <div id="time_display">00.00.00</div>
+                <div className={`${styles.btnContainer} row`}>
+                    <div className={`${styles.mediaBtns} col s4`}>
+                        <a href="" className="secondary-content"><i className="material-icons">stop</i></a>
+                        <a href="" className="secondary-content"><i
+                                className="material-icons">pause_circle_outline</i></a>
+                        <a href="" className="secondary-content"><i
+                                className="material-icons">play_circle_outline</i></a>
+                    </div>
+                    <div id="time_display" className='col s8'>00.00.00</div>
                 </div>
             </div>
 
--- src/components/Workspace/Workspace.jsx
+++ src/components/Workspace/Workspace.jsx
@@ -1,3 +1,4 @@
+import React from 'react'
 import styles from './Workspace.module.css'
 import TitleInput from './TitleInput/TitleInput'
 
