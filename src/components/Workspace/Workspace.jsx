b73bc33ab44251052aa41ad5dfb507c843741252
Wolferner <m.wolfern@yandex.com>
2023-10-30 15:13:36 +0200

    Workspace:: WsSearching - Start

--- src/components/Workspace/Workspace.jsx
+++ src/components/Workspace/Workspace.jsx
@@ -1,13 +1,22 @@
 import React from 'react'
 import styles from './Workspace.module.css'
 import WorkspaceHeader from './WorkspaceHeader/WorkspaceHeader'
+import WsSearching from './WsSearching/WsSearching'
 
 const Workspace = () =>{
 
     return(
-    <div >
-        <WorkspaceHeader/>
-    </div>
+        <div className='row'>
+            <div className={`${styles.wsHeader} col s12`} >
+                <WorkspaceHeader/>
+            </div>
+            <div className={`col s12`}>
+                <p>Favorits</p>
+            </div>
+            <div className={`col s12`}>
+                <WsSearching/>
+            </div>
+        </div>
     )
 }
 
